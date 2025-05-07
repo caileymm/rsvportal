aa0062a65d1ac8f499f61c79f1007c7829157372
Cailey Murad <cmmurad@ucsd.edu>
2025-05-07 15:52:02 -0700

    created tabs

---
+++ script.js
@@ -0,0 +1,7 @@
+function openTab(tabName) {
+    var tabs = document.getElementsByClassName('tab');
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].style.display = 'none';
+    }
+    document.getElementById(tabName).style.display = 'block';
+}
