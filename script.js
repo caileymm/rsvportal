58e2192d629cd5976c8ab6c7879478912371c28a
Cailey Murad <cmmurad@ucsd.edu>
2025-05-07 18:02:51 -0700

    edited layout, added count details

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ function openTab(tabName) {
     }
     document.getElementById(tabName).style.display = 'block';
 }
+
+let clicked = false;
+document.addEventListener('DOMContentLoaded', function() {
+  document.body.addEventListener('click', function(e) {
+      if (!clicked && e.target.classList.contains('rsvp-button')) {
+          const button = e.target;
+          const counter = button.closest('.event-card').querySelector('.rsvp-count');
+          let currentCount = parseInt(counter.textContent);
+          counter.textContent = currentCount + 1;
+          
+          button.textContent = "RSVPed!";
+          clicked = true;
+      }
+  });
+});
