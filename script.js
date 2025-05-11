76560d2e79f5f50022330c0aec3f68522ed00877
caileymurad <caileymurad@gmail.com>
2025-05-11 23:18:52 +0000

    connected to firebase

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
+// initialize firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyA5EAy2efIFrhPxIMKhVrshqEpEJIxyknY",
+  authDomain: "rsvp-portal.firebaseapp.com",
+  projectId: "rsvp-portal",
+  storageBucket: "rsvp-portal.firebasestorage.app",
+  messagingSenderId: "992133393969",
+  appId: "1:992133393969:web:14611e57a5b6389a5c2f10",
+  measurementId: "G-TKH7Y7G4G2"
+};
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// function to open a specific tab and hide others
 function openTab(tabName) {
-    var tabs = document.getElementsByClassName('tab');
-    for (var i = 0; i < tabs.length; i++) {
-      tabs[i].style.display = 'none';
-    }
-    document.getElementById(tabName).style.display = 'block';
+  // get all elements with the class 'tab'
+  var tabs = document.getElementsByClassName('tab');
+  // iterate over each tab and hide it by setting its display style to 'none'
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = 'none';
+  }
+  // get the element with the specified tabName and display it by setting its display style to 'block'
+  document.getElementById(tabName).style.display = 'block';
+}
+
+
+// function to increment the RSVP count when a button is clicked
+// also changes the button text to "RSVPed!" to indicate the user has RSVPed
+function incRSVPCount(button) {
+  // if button text is already "RSVPed!", then return
+  if (button.textContent === "RSVPed!") {
+      return;
+  }
+
+  // if not, then increment the count and update the button
+  // find the closest parent element with the class 'event-card', then find the element within that parent that has 
+  // the class 'rsvp-count'; this is the element displaying the RSVP count
+  const counter = button.closest('.event-card').querySelector('.rsvp-count');
+  // get the current count from the counter element and convert it to an integer to perform arithmetic
+  let currentCount = parseInt(counter.textContent);
+  counter.textContent = currentCount + 1;
+
+  button.textContent = "RSVPed!";
 }
