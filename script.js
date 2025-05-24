83a1f5bfa0d6fe7708ac4fa11a6f83848d44ef38
caileymurad <caileymurad@gmail.com>
2025-05-24 01:51:39 +0000

    get event data from db and show events and styling

--- script.js
+++ script.js
@@ -11,30 +11,84 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
-// TO DO: complete implementation of addEvent() (!!)
-function addEvent() {
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
+var eventIds = [];
+
+// TO DO: complete implementation of setEventCard() (!!)
+function setEventCard() {
   db.collection("eventCards").add({
-      eventTitle: eventTitle,
-      eventDescription: eventDescription,
-      eventOrg: eventOrg,
-      eventDate: eventDate,
-      eventTime: eventTime,
-      eventLocation: eventLocation,
-      eventImage: eventImage,
-      rsvpCount: rsvpCount
+    //eventDescription: eventDescription,
+    //eventOrg: eventOrg,
+    //eventDate: eventDate,
+    //eventTime: eventTime,
+    //eventLocation: eventLocation,
+    //eventImage: eventImage,
+    //rsvpCount: rsvpCount
   })
-  .then((docRef) => {
-      console.log("Document written with ID: ", docRef.id);
-  })
-  .catch((error) => {
-      console.error("Error adding document: ", error);
+  // TO DO: catch errors
+}
+
+function getEventCards() {
+  // db.collection("eventCards").get(): gets a reference to the 'eventCards' collection and fetches all the documents in it
+  // .then()((querySnapshot) => {...)): executed when the asynchronous operation (getting the document) is successful; 'querySnapshot' is an object that contains the results of the query
+  // querySnapshot.docs.forEach((doc) => { ... }): querySnapshot.docs is an arry of document snapshots; forEach() method is used to loop through each doc in this array
+  db.collection('eventCards').get().then((querySnapshot) => {
+    const eventsContainer = document.getElementById('events-container');
+    querySnapshot.docs.forEach((doc) => {
+      const eventData = doc.data();
+      
+      eventIds.forEach((id) => {
+        if (id === doc.id) {
+          let counter = eventsContainer.getAttribute('event-card-id', doc.id).querySelector('.rsvp-count');
+          let currentCount = parseInt(counter.textContent);
+          counter.textContent = eventData.eventRSVPCount;
+          return;
+        }
+      });
+
+      // create a new div element for the event card
+      const eventCard = document.createElement('div');
+      eventCard.classList.add('event-card');
+      eventCard.setAttribute('event-card-id', doc.id);
+
+      eventCard.innerHTML = `
+        <div style="float: left;">
+          <img class = "event-img" src=${eventData.eventImg} style="width: 200px; height: 250px">
+        </div>
+        <div style="margin-left: 225px">
+          <h2 class = "event-name"> ${eventData.eventName} </h2>
+          <div class = "event-desc"> ${eventData.eventDesc} </div>
+          <div class = "event-org">  ${eventData.eventOrg} </div>
+          <div class = "event-date"> ${eventData.eventDate}  </div>
+          <div class = "event-time"> ${eventData.eventTime} </div>
+          <div class = "event-loc"> ${eventData.eventLoc} </div>
+          <div style="position: absolute; top: 0; right: 0; padding: 15px 15px">
+            <div style="float: left; margin-top: 3px; font-size: 20px"> <span class="rsvp-count">${eventData.eventRSVPCount} </span> </div>
+            <img style="width: 20px; margin-left: 5px" src="assets/sungod.jpg">
+          </div>
+          <button class = "button" type="button" onclick="incRSVPCount(this)" style="margin-top: 20px"> RSVP! </button>
+        </div>
+      `;
+
+      eventsContainer.appendChild(eventCard);
+      eventIds.push(doc.id);
+    });
   });
 }
 
+getEventCards();
+
+/*
 // TEST (DELETE LATER !!)
-db.collection("testCollection").add({
-    testField: "testValue",
-    anotherField: "anotherValue"
+db.collection("eventCards").add({
+  eventName: "Sun God Festival 2025",
+  eventDesc: "UCSD's biggest annual music festival featuring live performances, food trucks, and more!",
+  eventOrg: "AS UCSD",
+  eventDate: "May 3rd, 2025",
+  eventTime: "12:00 PM to 6:00 PM",
+  eventLoc: "RIMAC Field",
+  eventImg: "assets/sungod-festival.jpg",
+  eventRSVPCount: "0"
 })
 .then((docRef) => {
     console.log("Document written with ID: ", docRef.id);
@@ -42,7 +96,7 @@ db.collection("testCollection").add({
 .catch((error) => {
     console.error("Error adding document: ", error);
 });
-
+*/
 
 // function to open a specific tab and hide others
 function openTab(tabName) {
@@ -72,6 +126,18 @@ function incRSVPCount(button) {
   // get the current count from the counter element and convert it to an integer to perform arithmetic
   let currentCount = parseInt(counter.textContent);
   counter.textContent = currentCount + 1;
+  const eventCardId = button.closest('.event-card').getAttribute('event-card-id');
+
+  // update the document in Firestore with the new RSVP count
+  db.collection("eventCards").doc(eventCardId).update({
+    eventRSVPCount: currentCount + 1
+  })
+  .then(() => {
+    console.log("Document successfully updated!");
+  })
+  .catch((error) => {
+    console.error("Error updating document: ", error);
+  });
 
   button.textContent = "RSVPed!";
 }
